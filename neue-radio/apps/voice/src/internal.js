10c65255793180e3226607c6a400bd6cfc1c4231
Libby Miller <libby.miller@bbc.co.uk>
2019-05-24 17:21:13 +0100

    Tweaks to voice file

--- neue-radio/apps/voice/src/internal.js
+++ neue-radio/apps/voice/src/internal.js
@@ -36,26 +36,26 @@ const main = async () => {
             websocket.publish({ topic: `robot/command/weather-overcast` });
 
           } else if (intent == 'radio_six_music'){
-            websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'i will play bbc radio six music when you learn to say please' } });
-            websocket.publish({ topic: `robot/command/turn-away` });
+           // websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'i will play bbc radio six music when you learn to say please' } });
+            websocket.publish({ topic: `robot/command/six-music` });
 
           } else if (intent == 'radio_six_music_please'){
-            websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'my pleasure. you have very good taste' } });
+           // websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'my pleasure. you have very good taste' } });
             websocket.publish({ topic: `robot/command/six-music-joy` });
 
           } else if (intent == 'radio_one'){
-            websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'if you must listen to that you could at least ask nicely' } });
+           // websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'if you must listen to that you could at least ask nicely' } });
             websocket.publish({ topic: `robot/command/radio-one-reluctance` });
 
-          } else if (intent == 'radio_one_please'){
-            websocket.publish({ topic: `speech/command/speak`, payload: { utterance: 'i am not a fan of bbc radio one but since you asked politely' } });
-            websocket.publish({ topic: `robot/command/radio-one-please` });
 
           } else if (intent == 'breaking_news'){
-            websocket.publish({ topic: `robot/command/good-news` });
+          websocket.publish({ topic: `robot/command/good-news` });
 
           } else if (intent == 'news_today'){
             websocket.publish({ topic: `robot/command/bad-news` });
+
+          } else if (intent == 'night_mode'){
+            websocket.publish({ topic: `robot/command/night-mode` });
           }
 
         }).catch((e) => {
